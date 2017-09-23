40cb9cf90b32183100f1aba1d9925fef993e61d7
Victor Wang <victor.wang@me.com>
2017-09-23 14:13:53 -0700

    fix denpendency issue

--- server/index.js
+++ server/index.js
@@ -10,15 +10,15 @@ app.use(express.static(__dirname + '/../client/dist'));
 
 app.post('/repos', (req, res) => {
   getReposByUsername(req.body.q)
-    .then(repos => Promise.all(repos.map(repo => save(repo)))
-                          .then(() => res.redirect('/repos')));
+    .then(repos => Promise.all(repos.map(repo => save(repo))))
+    .then(() => res.redirect('/repos'));
 })
 
 app.get('/repos', (req, res) => {
   find().then(repos => res.send(repos));
 })
 
-let port = process.env.PORT || 3000;
+let port = process.env.PORT || 5000;
 
 app.listen(port, function() {
   console.log(`listening on port ${port}`);
